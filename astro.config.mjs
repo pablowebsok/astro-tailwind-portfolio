7ab06d334b5576f9473b1badf886fa42a94907bf
pablomg-dev <pablomg.developer@gmail.com>
2026-08-09 22:23:21 -0300

    perf: optimize mobile PageSpeed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,12 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
 
 const siteUrl = "https://pablomgdev.xyz";
 
 export default defineConfig({
   integrations: [
     tailwind(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
   ],
   site: siteUrl,
   devToolbar: {
